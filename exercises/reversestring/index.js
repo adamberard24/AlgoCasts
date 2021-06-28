a4c4249298d2381b4df861315c2fd451eb053f74
Adam Schwartz <adamberard24@gmail.com>
2021-06-28 16:08:45 -0400

    did a test

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,9 +6,27 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {
-return str.split("").reverse().join("")
+// SOLUTION 1
+// function reverse(str) {
+// return str.split("").reverse().join("")
 
+// }
+
+
+// SOLUTION 2
+// function reverse(str) {
+//     let reversed = '';
+
+//     for (let character of str){
+//         reversed = character + reversed
+//     }
+//     return reversed
+// }
+
+function reverse(str) {
+  return str.split('').reduce((reversed, character) => {
+    return character  + reversed
+  }, '')
 }
 
 module.exports = reverse;
