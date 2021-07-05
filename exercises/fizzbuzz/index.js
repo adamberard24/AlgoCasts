39d7750815ad4ee8976202fe39f4b4b0efc9feb3
Adam Schwartz <adamberard24@gmail.com>
2021-07-05 14:36:55 -0400

    did the ol fizzbuzz

--- exercises/fizzbuzz/index.js
+++ exercises/fizzbuzz/index.js
@@ -12,6 +12,42 @@
 //   4
 //   buzz
 
-function fizzBuzz(n) {}
+// modulo operator!
+
+function fizzBuzz(n) {
+
+for (let i = 1; i <= n; i++ ) {
+    // is the number a multiple of 3 and 5?
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log('fizzbuzz');
+    } else if (i % 3 === 0) {
+        //is the number a multiple of 3
+        console.log("fizz")
+    } else if (i % 5 === 0){
+        console.log ("buzz")
+    } else {
+        console.log(i)
+    }
+}
+}
 
 module.exports = fizzBuzz;
+
+// MY SOLUTION THAT DID NOT WORK
+// function fizzBuzz(n) {
+
+//     for (let i = n - n + 1; i = n; i++ ) {
+//         if (i % 3 === 0) {
+//             prints ("fizz")
+//         } else if (i % 5 === 0) {
+//             prints ("buzz")
+//         } else if (i % 5 === 0 && i % 3 === 0){
+//             prints ("fizzbuzz")
+//         } else {
+//             prints (i)
+//         }
+//     }
+    
+    
+    
+//     }
