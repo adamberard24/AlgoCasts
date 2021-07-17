51ff68f99f4638302b1d592a28f53bc8e079a82b
Adam Schwartz <adamberard24@gmail.com>
2021-07-16 20:46:04 -0400

    Did one

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,52 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+// RECURSIVE SOLUTION (AKA SOLUTION 2)
+    if (n === row) {
+        return;
+    }
+
+    if (n === stair.length) {
+        console.log(stair)
+      return steps(n, row + 1)
+    }
+
+    if (stair.length <= row) {
+        stair += '#'
+    } else {
+        stair += ' '
+    }
+    steps(n, row, stair)
+
+}
 
 module.exports = steps;
+
+// MY SOLUTION
+// function steps(n) {
+
+//     for (i = 1; i <= n; i++){
+
+//         let pound = '#'
+//         let space = ' '
+//         console.log(pound.repeat(i) + space.repeat(n - i))
+        
+//     }
+// }
+
+// HIS SOLUTION 1
+// function steps(n) {
+//     for (let row = 0;row < n; row ++) {
+//         let stair = ''
+   
+//         for (let column = 0; column < n; column++) {
+//             if (column <= row) {
+//                 stair += '#'
+//             } else {
+//                 stair += ' '
+//             }
+//         }
+//         console.log(stair)
+//     }
+//    }
