45f82cedb0ff4743c885487e958899595b9e426b
Adam Schwartz <adamberard24@gmail.com>
2021-07-19 10:55:24 -0400

    did it

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,51 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+
+
+function pyramid(n, row = 0, level = '') {
+
+    
+        // RECURSIVE SOLUTION (AKA SOLUTION 2)
+            if (n === row) {
+                return;
+            }
+        
+            if (2 * n - 1 === level.length) {
+                console.log(level)
+              return pyramid(n, row + 1)
+            }
+        
+            const midpoint = Math.floor((2 * n -1) / 2)
+            let add
+            if (midpoint - row <= level.length && midpoint + row >= level.length) {
+                add = '#'
+            } else {
+                add = ' '
+            }
+            pyramid(n, row, level + add)
+        
+        
+}
+
 
 module.exports = pyramid;
+
+// ITERATIVE SOLUTION
+// function pyramid(n) {
+
+//     const midpoint = Math.floor((2*n - 1) / 2)
+
+//     for (let row = 0; row < n; row++) {
+//         let level = '';
+
+//         for (let column = 0; column < 2 * n - 1; column++){
+//             if (midpoint - row <= column && midpoint + row >= column) {
+//                 level += '#'
+//             } else {
+//                 level += ' '
+//             }
+//         }
+//         console.log(level)
+//     }
+// }
